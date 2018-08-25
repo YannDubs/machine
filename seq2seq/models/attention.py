5da4b526914ac212a3099f882f8e0ac1fc78ed36
Yann Dubois <yanndubois96@gmail.com>
2018-08-25 04:43:14 +0200

    STY: Uses the usual conventions for attnetion.

--- seq2seq/models/attention.py
+++ seq2seq/models/attention.py
@@ -128,7 +128,7 @@ class ContentAttention(nn.Module):
         # indeed max < logsumexp < max + log(n)
         # so if want to be sure than never too far from real max can use
         # max - log(n)/2 < logsumexp - log(n)/2 < max + log(n)/2
-        #approx_max_logit = approx_max_logit - math.log(logits.size(-1)) / 2
+        # approx_max_logit = approx_max_logit - math.log(logits.size(-1)) / 2
 
         confidence = self.maxlogit_to_conf(approx_max_logit)
 
@@ -140,12 +140,12 @@ class ContentAttention(nn.Module):
         """
         Set method to compute attention
         """
-        if method == 'mlp':
-            method = MLPAttn(dim)
-        elif method == 'concat':
-            method = Concat(dim)
+        if method == 'multiplicative':
+            method = MultiplicativeAttn(dim)
+        if method == 'additive':
+            method = AdditiveAttn(dim)
         elif method == 'dot':
-            method = Dot()
+            method = ScaledDotAttn()
         elif method == 'hard':
             method = HardGuidance()
         else:
@@ -169,54 +169,7 @@ class ContentAttention(nn.Module):
                 additional["test"][keys] = values
 
 
-class Concat(nn.Module):
-    """
-    Implements the computation of attention by applying an
-    MLP to the concatenation of the decoder and encoder
-    hidden states.
-    """
-
-    def __init__(self, dim):
-        super(Concat, self).__init__()
-        self.mlp = nn.Linear(dim * 2, 1)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        linear_init(self.mlp)
-
-    def forward(self, decoder_states, encoder_states):
-        # apply mlp to all encoder states for current decoder
-
-        # decoder_states --> (batch, dec_seqlen, hl_size)
-        # encoder_states --> (batch, enc_seqlen, hl_size)
-        batch_size, enc_seqlen, hl_size = encoder_states.size()
-        _, dec_seqlen, _ = decoder_states.size()
-
-        # (batch, enc_seqlen, hl_size) -> (batch, dec_seqlen, enc_seqlen, hl_size)
-        encoder_states_exp = encoder_states.unsqueeze(1)
-        encoder_states_exp = encoder_states_exp.expand(batch_size, dec_seqlen, enc_seqlen, hl_size)
-
-        # (batch, dec_seqlen, hl_size) -> (batch, dec_seqlen, enc_seqlen, hl_size)
-        decoder_states_exp = decoder_states.unsqueeze(2)
-        decoder_states_exp = decoder_states_exp.expand(batch_size, dec_seqlen, enc_seqlen, hl_size)
-
-        # reshape encoder and decoder states to allow batchwise computation. We will have
-        # batch_size x enc_seqlen x dec_seqlen batches. So we apply the Linear
-        # layer for each of them
-        decoder_states_tr = decoder_states_exp.contiguous().view(-1, hl_size)
-        encoder_states_tr = encoder_states_exp.contiguous().view(-1, hl_size)
-
-        mlp_input = torch.cat((encoder_states_tr, decoder_states_tr), dim=1)
-
-        # apply mlp and respape to get in correct form
-        mlp_output = self.mlp(mlp_input)
-        attn = mlp_output.view(batch_size, dec_seqlen, enc_seqlen)
-
-        return attn
-
-
-class Dot(nn.Module):
+class ScaledDotAttn(nn.Module):
     """
     Implements the computation of attention by using a scaled attention just liek
     in "attention is all you need". Scaling can help when dimension is large :
@@ -224,27 +177,52 @@ class Dot(nn.Module):
     """
 
     def __init__(self):
-        super(Dot, self).__init__()
+        super().__init__()
 
-    def forward(self, decoder_states, encoder_states):
-        attn = torch.bmm(decoder_states, encoder_states.transpose(1, 2))
-        attn = attn / math.sqrt(decoder_states.size(-1))
-        return attn
+    def forward(self, queries, keys):
+        logits = torch.bmm(queries, keys.transpose(1, 2))
+        logits = logits / math.sqrt(queries.size(-1))
+        return logits
 
 
-class MLPAttn(nn.Module):
+class MultiplicativeAttn(nn.Module):
+    """
+    Implements the computation of attention by using a scaled attention just liek
+    in "attention is all you need". Scaling can help when dimension is large :
+    making sure that there are no  extremely small gradients
+    """
 
     def __init__(self, dim):
-        super(MLPAttn, self).__init__()
-        self.mlp = nn.Linear(dim * 2, dim)
-        self.activation = nn.ReLU()
-        self.out = nn.Linear(dim, 1)
+        super().__init__()
+        self.linear = nn.Linear(dim, dim)
+        self.scaled_dot = ScaledDotAttn()
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        linear_init(self.mlp, activation="relu")
-        linear_init(self.out)
+        linear_init(self.linear)
+
+    def forward(self, queries, keys):
+        transformed_queries = self.linear(queries)
+        logits = self.scaled_dot(transformed_queries, keys)
+        return logits
+
+
+class AdditiveAttn(nn.Module):
+    """
+    Implements additive attention as seen in Bahdanau et al. :
+    "Neural Machine Translation by Jointly Learning to Align and Translate"
+    """
+
+    def __init__(self, dim):
+        super().__init__()
+        self.mlp = MLP(dim * 2, dim, 1, activation=nn.ReLU)
+        # should try nn.Tanh
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.apply(weights_init)
 
     def forward(self, decoder_states, encoder_states):
         # apply mlp to all encoder states for current decoder
@@ -271,12 +249,10 @@ class MLPAttn(nn.Module):
         mlp_input = torch.cat((encoder_states_tr, decoder_states_tr), dim=1)
 
         # apply mlp and reshape to get in correct form
-        mlp_output = self.mlp(mlp_input)
-        mlp_output = self.activation(mlp_output)
-        out = self.out(mlp_output)
-        attn = out.view(batch_size, dec_seqlen, enc_seqlen)
+        logits = self.mlp(mlp_input)
+        logits = logits.view(batch_size, dec_seqlen, enc_seqlen)
 
-        return attn
+        return logits
 
 
 class HardGuidance(nn.Module):
